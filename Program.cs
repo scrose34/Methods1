de34da54ca4df3dd58a18277665510794a18570b
Spencer <scrose34@gmail.com>
2021-02-05 21:04:10 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello" + name);
+        }
+    }
+}
